dc771ca1dd9f55891e00fec864f8ef1dcbefbd71
Srikanth Bharadwaj <srikanth.bharadwaj.documents@gmail.com>
2022-05-08 20:10:05 -0700

    Create login form
    Create the login html page which includes a form for the user to enter their email address and password.

--- src/website/auth.py
+++ src/website/auth.py
@@ -5,7 +5,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login page placeholder</p>"
+    return render_template("login.html")
 
 
 @auth.route('/logout')
