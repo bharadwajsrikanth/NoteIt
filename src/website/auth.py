25696086e8d6d64b8dcf91b899b8c864776f64db
Srikanth Bharadwaj <srikanth.bharadwaj.documents@gmail.com>
2022-05-09 18:36:54 -0700

    Accept POST requests and add flash messages
    Allow the login and register pages to send POST requests to our servers. This commit only allows the POST request. The data is not handled yet. Add sanity checks on the inputs in register page and throw flash messages.

--- src/website/auth.py
+++ src/website/auth.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint
+from flask import render_template
+from flask import request
+from flask import flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -13,6 +16,24 @@ def logout():
     return "<p>Logout page placeholder</p>"
 
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be greater than 7 character.', category='error')
+        else:
+            '''Add user to DB'''
+            flash('Account successfully created.', category='success')
+
     return render_template("register.html")
