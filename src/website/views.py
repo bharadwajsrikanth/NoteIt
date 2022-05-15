621d9df1ed313b77febb282a9cb28cd5e29465da
Srikanth Bharadwaj <srikanth.bharadwaj.documents@gmail.com>
2022-05-14 18:15:14 -0700

    Add Note functionality
    Allow logged in user to be able to add notes and delete notes.

--- src/website/views.py
+++ src/website/views.py
@@ -1,12 +1,42 @@
 from flask import Blueprint
+from flask import flash
+from flask import jsonify
 from flask import render_template
+from flask import request
 from flask_login import login_required
 from flask_login import current_user
+import json
+
+from . import db
+from .models import Note
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+        if len(note) < 1:
+            flash('Note is too short.', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note successfully added.', category='success')
+
     return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    note_id = note['noteId']
+    note = Note.query.get(note_id)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
